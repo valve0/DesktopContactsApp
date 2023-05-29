bce6e502d0f0c294037691e76606d1eb3b7c1e20
Tom <tw12553@gmail.com>
2023-05-29 09:44:21 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Contact> contacts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            contacts = new List<Contact>();
+
+            ReadDatabase();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+
+        }
+
+        void ReadDatabase()
+        {
+            
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = conn.Table<Contact>().ToList().OrderBy(c => c.Name).ToList();
+
+            }
+
+            if (contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+           
+            if (searchTextBox != null)
+            {
+                var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+
+                // This is what the LINQ query above accomplishes with a SQL query
+                //var filteredList2 = (from c2 in contacts
+                //                    where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+                //                    orderby c2.Name
+                //                    select c2).ToList();
+
+                contactsListView.ItemsSource = filteredList;
+            }
+
+            
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+            if (selectedContact!=null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+
+                ReadDatabase();
+            }
+            
+        }
+    }
+}
---
+++ NewContactWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Save a contact
+
+            //Create objects with inputted details
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                PhoneNumber = phoneTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+
+        }
+    }
+}
